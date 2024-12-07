8e29ff23c0f6611e9245f24f5e0309d9ac1c43a9
nainshi880 <nainshijaiswal24@gmail.com>
2024-12-07 08:53:12 +0530

    making structure and add styling

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById("container");
+const bucket =document.getElementById("bucket");
+let scoreDisplay=document.getElementById("score");
+let heartDisplay=document.getElementById("life");
+const startScreen=document.getElementById("startScreen");
+const endScreen=document.getElementById("endScreen");
+const startButton=document.getElementById("btn1");
+const endButton=document.getElementById("btn2");
+const finalScore =document.getElementById("finalScore");
+const dropSound=document.getElementById("dropSound");
+const missSound=document.getElementById("missSound");
